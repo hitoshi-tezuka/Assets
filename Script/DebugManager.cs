10ee2a396f9c2a7ff09fc785252d02d6c00bc506
tedeuka <songofsaya.cuteful@gmail.com>
2018-07-03 23:19:05 +0900

    photon

---
+++ Script/DebugManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugManager : MonoBehaviour {
+
+    [SerializeField] GameObject m_BattleManager;
+    [SerializeField] GameObject m_PhotonManager;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(10, 10, 150, 100), "BattleManager"))
+        {
+            m_BattleManager.SetActive(!m_BattleManager.activeSelf);
+        }
+        if (GUI.Button(new Rect(10, 150, 150, 100), "PhotonManager"))
+        {
+            m_PhotonManager.SetActive(!m_PhotonManager.activeSelf);
+        }
+
+    }
+
+}
